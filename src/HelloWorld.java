969af45d84df04231e88e18614c7de6bb02bde59
Gerald Montero <geraldmontero12@gmail.com>
2022-02-15 14:52:39 -0600

    tyoes and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,54 @@ public class HelloWorld {
     public static void main (String[] args){
         //print helloworld to console
         System.out.println("Hello World");
+        // 1.My favorite number
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        // 2.My string
+        String myString = "3.14159";
+        System.out.println(myString);
+        //3.Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        float  myNumber = 314;
+        System.out.println(myNumber);
+        //10.
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
 
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        //12
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+        //13
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//
+//                int x = 4;
+//                //x = x + 5;
+//                x += 5;
+//        System.out.println(x);
+
+//                int x = 3;
+//                int y = 4;
+//                //y = y * x;
+//                y *= x;
+//        System.out.println(y);
+
+
+                int x = 10;
+                int y = 2;
+                //x = x / y;
+                    x /=y;
+        System.out.println(x);
+                //y = y - x;
+                y -= x;
+        System.out.println(y);
 
     }
 
