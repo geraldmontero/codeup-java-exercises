b028102c5f76e7feec380186a26a4698b5b43bf9
Gerald Montero <geraldmontero12@gmail.com>
2022-02-15 11:31:08 -0600

    Hello World

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,11 @@
 public class HelloWorld {
+        // method - similar to function in javascript
+        //main method -> specific method in java tha acts as our entry point when running java
+    public static void main (String[] args){
+        //print helloworld to console
+        System.out.println("Hello World");
+
+
+    }
+
 }
-gti
