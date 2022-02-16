5680c57c7da539fdec1f0ee4cbd4729d69588403
Gerald Montero <geraldmontero12@gmail.com>
2022-02-16 11:39:14 -0600

    console io

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+        double pi = 3.14159;
+        String name = "Pie";
+        System.out.printf("The value of pi is approximately",+ pi);
+
+
+    }
+
+
+
+}
