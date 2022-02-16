7684c79d5d044efcc25adc7a8275c246c5ea86f6
Gerald Montero <geraldmontero12@gmail.com>
2022-02-16 12:04:59 -0600

    update

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,9 +3,22 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-        double pi = 3.14159;
-        String name = "Pie";
-        System.out.printf("The value of pi is approximately",+ pi);
+            Scanner scanner = new Scanner(System.in);
+//        double pi = 3.14159;
+//        String name = "Pie";
+//        System.out.printf("The value of pi is approximately",+ pi);
+        System.out.print("enter and interger: ");
+        int userInput = scanner.nextInt();
+        System.out.println(userInput);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter 3 word:");
+//        String userIn = scanner.next();
+//        String userIn2 = scanner.next();
+//        String userIn3 = scanner.next();
+//
+//        System.out.printf(userIn + "%n" + userIn2 + "%n" + userIn3);
+
 
 
     }
