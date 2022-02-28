64d3f948e1be47548ddf97a160c46ff478dee783
Gerald Montero <geraldmontero12@gmail.com>
2022-02-28 09:25:24 -0600

    morning push

---
+++ src/util/input.java
@@ -0,0 +1,15 @@
+package util;
+
+public class input {
+    private class scanner {
+
+        //String getString()
+        //boolean yesNo()
+        //int getInt(int min, int max)
+        //int getInt()
+        //double getDouble(double min, double max)
+        //double getDouble()
+
+
+    }
+}
