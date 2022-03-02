b23f36dc12cff58e270e260de2316bb37306e7e8
Gerald Montero <geraldmontero12@gmail.com>
2022-03-02 12:28:27 -0600

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,44 +1,41 @@
 import java.util.Arrays;
+
 public class ArraysExercises {
-    public static Person[] addPerson(Person[] people, Person single){
 
+    // Array for adding new person
+    public static Person[] addPerson(Person[] people, Person single) { //  Person[] people = type of parameter then name
+// Person[] = whatever you want to return   addPerson = name of variable
+        // copy array and adding + 1
+        Person[] newMember = Arrays.copyOf(people, people.length + 1);
 
+        // spot in the array
+        newMember[newMember.length - 1] = single;
+
+        for(Person personR: newMember) {
+            System.out.println(personR.getName());
+        }
+        return newMember;
     }
-
-
-
 
     public static void main(String[] args) {
-
+//        // What happens when you run the following code? Why is Arrays.toString necessary?
 //        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(numbers);
-       Person[] people = new Person[3];
-
-       people[0] = new Person("Gerald");
-       people[1] = new Person("robert");
-       people[2] = new Person("andrew");
+////        System.out.println(numbers); // [I@6bdf28bb
 
 
+        // 1 .
 
+        // array object
+        Person[] people = new Person[3];
 
+        people[0] = new Person("Kyle");
+        people[1] = new Person("Gerald");
+        people[2] = new Person("Andrew");
 
+        // Single person object
+        Person varMe = new Person("Robert");
+
+        // calling object/ single object to print
+        addPerson(people, varMe);
     }
-
-
-            };
-
-
-        //Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
-        //
-        //Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
-
-
-
-
-    }
-
-
-
-
-
 }
