f4a9131d9c85ead1f06f19c03e3b609a2fed6930
Gerald Montero <geraldmontero12@gmail.com>
2022-03-03 16:09:35 -0600

    update

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package movies;
+
+public class MoviesApplication {
+//Give the user a list of options for viewing all the movies or viewing movies by category.
+//Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
+//If a category is selected, only movies from that category should be displayed.
+//Your application should continue to run until the user chooses to exit.
+public static void main(String[] args) {
+    System.out.println("welcome to the movie app");
+    System.out.println("Please make a selection");
+    System.out.println();
+}
+
+}
